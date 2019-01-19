f58bbbac002401b488ffcd3ae5a8204233e6600e
zzzzeu <zouanei@outlook.com>
2019-01-19 21:47:46 +0800

    chapter 1: introduction to algorithms

---
+++ Chapter 1 I Introduction to Algorithms.playground/Pages/Running Time.xcplaygroundpage/Contents.swift
@@ -0,0 +1,7 @@
+// Big O notation
+
+//O(log n), also known as log time. Example: Binary search.
+//O(n), also known as linear time. Example: Simple search.
+//O(n * log n). Example: A fast sorting algorithm, like quicksort
+//O(n2). Example: A slow sorting algorithm, like selection sort.
+//O(n!). Example: A really slow algorithm, like the traveling salesperson.
